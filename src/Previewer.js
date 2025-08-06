ad6b79bf5a960fd2d6b90b3b66946a9a17072a6b
Aman Asthana <120777911+amanasthana1111@users.noreply.github.com>
2025-08-06 17:36:34 +0530

    Update Previewer.js

--- src/Previewer.js
+++ src/Previewer.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import React, { useEffect, useRef } from "react";
 import { usePdf } from "./Pdfcontext";
-import  JSZip  from "jszip";
-import { saveAs } from "file-saver";
+// import  JSZip  from "jszip";
+// import { saveAs } from "file-saver";
 
 
 const Previewer = () => {
@@ -10,18 +10,18 @@ const Previewer = () => {
   const data = Resume.html
 
   if(data==undefined){return}
-  const downloadZip = () => {
+  // const downloadZip = () => {
     
-    const zip = new JSZip();
+  //   const zip = new JSZip();
 
-    // Add files to the zip
-    zip.file("index.html", data || "");
+  //   // Add files to the zip
+  //   zip.file("index.html", data || "");
 
-    // Generate the zip and trigger download
-    zip.generateAsync({ type: "blob" }).then((content) => {
-      saveAs(content, "Portfoilo-website.zip");
-    });
-  };
+  //   // Generate the zip and trigger download
+  //   zip.generateAsync({ type: "blob" }).then((content) => {
+  //     saveAs(content, "Portfoilo-website.zip");
+  //   });
+  // };
   
   const iframeRef = useRef(null);
    useEffect(() => {
@@ -36,14 +36,14 @@ const Previewer = () => {
   }, [data]);
   return (
    <>
-  <div className="flex justify-end p-4 bg-[#FAF4F3] border-b shadow-sm">
-    <button
-      onClick={downloadZip}
-      className="bg-gradient-to-r from-red-500 to-red-600 text-white font-semibold px-6 py-3 rounded-lg shadow-lg hover:from-red-600 hover:to-red-700 hover:shadow-xl transform hover:scale-105 transition duration-300 ease-out"
-    >
-      Download Code
-    </button>
-  </div>
+  // <div className="flex justify-end p-4 bg-[#FAF4F3] border-b shadow-sm">
+  //   <button
+  //     onClick={downloadZip}
+  //     className="bg-gradient-to-r from-red-500 to-red-600 text-white font-semibold px-6 py-3 rounded-lg shadow-lg hover:from-red-600 hover:to-red-700 hover:shadow-xl transform hover:scale-105 transition duration-300 ease-out"
+  //   >
+  //     Download Code
+  //   </button>
+  // </div>
 
   <div className="w-full h-[100vh] border rounded overflow-hidden shadow-lg">
     <iframe
